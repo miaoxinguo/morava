5d7a5924af6b691c51ac1a8acc419472a3ea8c62
miaoxinguo <miaoxinguo2002@163.com>
2017-02-27 00:04:01 +0800

    初始化

---
+++ src/main/java/io/github/miaoxinguo/sbs/constant/Constants.java
@@ -0,0 +1,18 @@
+package io.github.miaoxinguo.sbs.constant;
+
+/**
+ * 通用常量
+ */
+public final class Constants {
+
+    private Constants() {}
+
+    // 编码
+    public static final String ENCODING_DEFAULT = "UTF-8";
+
+    // 分隔符
+    public static final String SEPARATOR_COMMA = ",";
+    public static final String SEPARATOR_SPACE = " ";
+
+
+}
